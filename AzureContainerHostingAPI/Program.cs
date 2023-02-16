2bf3e2a4323866a287742774719cd976bbd3f0a0
frq210 <anujkumar.yadav@afry.com>
2023-02-16 15:39:41 +0100

    Changes

--- AzureContainerHostingAPI/Program.cs
+++ AzureContainerHostingAPI/Program.cs
@@ -1,3 +1,5 @@
+using WatchDog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddWatchDogServices(opt  => 
+{
+    opt.IsAutoClear = true; // Auto clear after certain Period
+    opt.ClearTimeSchedule = WatchDog.src.Enums.WatchDogAutoClearScheduleEnum.Weekly; // Clear Period 
+
+});
 
 var app = builder.Build();
 
